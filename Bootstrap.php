a2143782b0e09c499eaeca2393ac8c1fd3cdf479
Roman Sable <sable.lair@gmail.com>
2018-02-26 11:55:04 +0300

    fix i18n bug

--- Bootstrap.php
+++ Bootstrap.php
@@ -8,6 +8,7 @@
 
 namespace pamfax\api;
 
+use yii\helpers\ArrayHelper;
 use yii\base\BootstrapInterface;
 
 class Bootstrap implements BootstrapInterface {
@@ -16,6 +17,11 @@ class Bootstrap implements BootstrapInterface {
     public function bootstrap( $app ) {
 
         $app->i18n->translations['pamfax*'] = require( __DIR__ . '/locale/translate.php' );
+        $components = \Yii::$app->getComponents();
+        $translations = ( empty( $components['i18n']['translations'] ) )? [] : $components['i18n']['translations'];
+        $translations['pamfax*'] = ArrayHelper::toArray(\Yii::$app->i18n)['translations']['pamfax*'];
+        $components['i18n']['translations'] = $translations;
+        \Yii::$app->setComponents( $components );
 
         require_once( dirname(__FILE__) . "/lib/apiclient.class.php");
         require_once( dirname(__FILE__) . "/lib/apierror.class.php");
